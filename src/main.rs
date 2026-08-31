14891e19371fd3af0568e81028d30fd886f44774
Borys Kopeć <boryskopec00@gmail.com>
2026-08-31 14:35:14 +0200

    Backup first drafts of the code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    trpl::block_on(async {
+        let (tx, mut rx) = trpl::channel();
+
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("future"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            trpl::sleep(Duration::from_millis(500)).await;
+        }
+
+        while let Some(value) = rx.recv().await {
+            println!("received '{value}'");
+        }
+    });
 }
