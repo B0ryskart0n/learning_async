6e2935f8868bd0a1f4794b1dae7ddffaf5313280
Borys Kopeć <boryskopec00@gmail.com>
2026-09-08 23:39:33 +0200

    Decrease number of messages; introduce new sender task

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,20 @@ use std::time::Duration;
 
 fn main() {
     let (tx, mut rx) = trpl::channel();
+    let tx2 = tx.clone();
+
     // The `move` moves the `tx` ownership, so it gets dropped after finishing sending.
     // This makes the `rx` return `None` because senders are closed.
     let tx_fut = async move {
-        let vals = vec![
-            String::from("hi"),
-            String::from("from"),
-            String::from("the"),
-            String::from("future"),
-        ];
-
-        for val in vals {
+        for val in vec!["I", "love", "you"].into_iter().map(String::from) {
             trpl::sleep(Duration::from_millis(500)).await;
             tx.send(val).unwrap();
+        }
+    };
+    let tx2_fut = async move {
+        for val in vec!["this", "and", "that"].into_iter().map(String::from) {
+            trpl::sleep(Duration::from_millis(800)).await;
+            tx2.send(val).unwrap();
         }
     };
 
@@ -24,5 +25,5 @@ fn main() {
         }
     };
 
-    trpl::block_on(trpl::join(tx_fut, rx_fut));
+    trpl::block_on(async { trpl::join!(tx_fut, rx_fut, tx2_fut) });
 }
