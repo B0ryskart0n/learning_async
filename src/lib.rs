0b7bc1df748229614bdaad3f438fd7b7cfe597d6
Borys Kopeć <boryskopec00@gmail.com>
2026-09-13 11:16:32 +0200

    New blank functions for async

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+pub enum Either<A, B> {
+    Left(A),
+    Right(B),
+}
+
+pub fn or<A: Future, B: Future>(
+    _f1: A,
+    _f2: B,
+) -> impl Future<Output = Either<A::Output, B::Output>> {
+    async { todo!() }
+}
+pub fn and<A: Future, B: Future>(_f1: A, _f2: B) -> impl Future<Output = (A::Output, B::Output)> {
+    async { todo!() }
+}
+
 pub fn wait_for<T: Future>(future: T) -> T::Output {
-    todo!();
+    todo!()
 }
