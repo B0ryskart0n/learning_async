b05d2c8cc7846a44c914d44eab4f8a50aa710321
Borys Kopeć <boryskopec00@gmail.com>
2026-09-13 11:14:47 +0200

    Extend main with new testing future

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+use std::thread::sleep;
+use std::time::Duration;
+
 use learning_async::wait_for;
 
 fn main() {
     let trivial_future = async {
-        println!("I've been awaited!");
+        println!("Trivial future finishing.");
+    };
+    let less_trivial_future = async {
+        sleep(Duration::from_secs(1));
+        println!("Less trivial future finishing.");
     };
 
     wait_for(trivial_future);
+    wait_for(less_trivial_future);
 }
