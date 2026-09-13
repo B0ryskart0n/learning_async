7407900ee751fb7252149cf2791fad0b9974e23d
Borys Kopeć <boryskopec00@gmail.com>
2026-09-13 10:17:59 +0200

    Start implementing my runtime

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,9 @@
-use std::thread;
-use std::time::Duration;
-
-fn cpu_slow(name: &str, ms: u64) {
-    thread::sleep(Duration::from_millis(ms));
-    println!("Calculated '{name}' in {ms}ms.");
-}
-
-async fn io_slow(name: &str, ms: u64) {
-    trpl::sleep(Duration::from_millis(ms)).await;
-    println!("Wrote to disk '{name}' in {ms}ms.");
-}
+use learning_async::wait_for;
 
 fn main() {
-    let (tx, mut rx) = trpl::channel();
-    let tx2 = tx.clone();
-
-    // The `move` moves the `tx` ownership, so it gets dropped after finishing sending.
-    // This makes the `rx` return `None` because senders are closed.
-    let tx_fut = async move {
-        for val in vec!["I", "love", "you"].into_iter().map(String::from) {
-            trpl::sleep(Duration::from_millis(500)).await;
-            tx.send(val).unwrap();
-        }
-    };
-    let tx2_fut = async move {
-        for val in vec!["this", "and", "that"].into_iter().map(String::from) {
-            trpl::sleep(Duration::from_millis(800)).await;
-            tx2.send(val).unwrap();
-        }
+    let trivial_future = async {
+        println!("I've been awaited!");
     };
 
-    let rx_fut = async {
-        while let Some(value) = rx.recv().await {
-            println!("received '{value}'");
-        }
-    };
-
-    trpl::block_on(async { trpl::join!(tx_fut, rx_fut, tx2_fut) });
+    wait_for(trivial_future);
 }
